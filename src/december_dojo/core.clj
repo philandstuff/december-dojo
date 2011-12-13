26dc97e2c7879a320f5a34687c7238a0264f16b4
Philip Potter <philipgpotter@gmail.com>
2011-12-13 21:28:34 +0000

    reductions

--- src/december_dojo/core.clj
+++ src/december_dojo/core.clj
@@ -5,3 +5,14 @@
             (fn [& args]
               (vec (map #(apply % args) fns))))
 
+;; #60: reductions
+;; does correct calculation but fails for not being lazy
+(defn my-red
+          ([f coll]
+            (if (seq coll)
+                (cons (first coll) (map #(+ % (first coll)) (my-red f (rest (seq coll)))))
+                []))
+        ([f init coll]
+           (my-red f (cons init coll))))
+
+
