f4671005ebde259546f74a984e4461f419c4a056
Philip Potter <philipgpotter@gmail.com>
2011-12-13 20:38:06 +0000

    put the code into the code dir

--- project.clj
+++ project.clj
@@ -2,8 +2,3 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.2.1"]])
 
-;; #59: juxtaposition
-(defn juxt [& fns]
-            (fn [& args]
-              (vec (map #(apply % args) fns))))
-
--- src/december_dojo/core.clj
+++ src/december_dojo/core.clj
@@ -1,1 +1,7 @@
 (ns december-dojo.core)
+
+;; #59: juxtaposition
+(defn juxt [& fns]
+            (fn [& args]
+              (vec (map #(apply % args) fns))))
+
